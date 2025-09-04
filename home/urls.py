10233b77af7bab0d7c052031ee76d4a974b3ee56
johnk <djonte0@gmail.com>
2025-09-04 10:59:10 +0300

    minor bug fix

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns=[
 path('', views.Home, name='index'),
-path('homepage', views.homepage, name='homepage'),
+path('landing', views.homepage, name='homepage'),
 path('term_conditions', views.term_conditions, name='term_conditions'),
 path('privacy_policies', views.privacy_policies, name='privacy_policies'),
 ]
--- mpesa_integration/mpesa_credentials.py
+++ mpesa_integration/mpesa_credentials.py
@@ -28,7 +28,7 @@ def get_mpesa_access_token():
         
 class MpesaAccessToken:
     print(MpesaC2bCredential.api_URL, MpesaC2bCredential.consumer_key, MpesaC2bCredential.consumer_secret)
-    validated_mpesa_access_token = get_mpesa_access_token()
+    # validated_mpesa_access_token = get_mpesa_access_token()
 
 class LipanaMpesaPpassword:
     lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
